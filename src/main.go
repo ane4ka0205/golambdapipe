772f53c0a847220651be5387aa62857834488dd6
Anna Malanova <anna.malanova@slalom.com>
2019-10-22 21:26:12 -0500

    change3

---
+++ src/main.go
@@ -0,0 +1,19 @@
+  
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func helloworld() string {
+	return ("hello world")
+}
+
+func handler() {
+	fmt.Println(helloworld())
+}
+
+func main() {
+	lambda.Start(handler)
+}
